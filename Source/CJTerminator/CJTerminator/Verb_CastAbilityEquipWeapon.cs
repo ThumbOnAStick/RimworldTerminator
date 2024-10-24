8db6946ec6a54a05a9b5d9a028344c68e169b003
zch0713 <120911598+ThumbOnAStick@users.noreply.github.com>
2024-10-24 20:49:04 +1000

    Allow robots to equip weapons

---
+++ Source/CJTerminator/CJTerminator/Verb_CastAbilityEquipWeapon.cs
@@ -0,0 +1,73 @@
+﻿using RimWorld;
+using RimWorld.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using Verse;
+using Verse.AI;
+
+namespace CJTerminator
+{
+    public class Verb_CastAbilityEquipWeapon: Verb
+    {
+        protected override bool TryCastShot()
+        {
+            return true;
+        }
+
+        public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+        {
+            //bool newResult = false;
+            //if (target != null && target.HasThing && target.Thing.def.IsWeapon &&
+            //    ability.pawn.CanReach(target, PathEndMode.Touch, ability.pawn.NormalMaxDanger()))
+            //{
+            //    newResult = true;
+            //}
+
+
+            return target != null && target.Cell.TryGetFirstThing(map;
+           //     && target.HasThing && target.Thing.def.IsWeapon &&
+           //this.CasterPawn.CanReach(target, PathEndMode.Touch, this.CasterPawn.NormalMaxDanger());
+        }
+
+
+        public override bool MultiSelect
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+
+        public virtual ThingDef JumpFlyerDef
+        {
+            get
+            {
+                return ThingDefOf.PawnFlyer;
+            }
+        }
+
+
+
+        public override void OnGUI(LocalTargetInfo target)
+        {
+            if (this.ValidateTarget(target))
+            {
+                return;
+            }
+            GenUI.DrawMouseAttachment(TexCommand.CannotShoot);
+        }
+
+
+
+        private float cachedEffectiveRange = -1f;
+
+    }
+
+   
+}
