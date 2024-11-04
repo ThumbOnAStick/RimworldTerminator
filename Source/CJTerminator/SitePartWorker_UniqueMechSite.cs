44f977ec7f375d7ef9f21c24cffeee23117f2d6f
zch0713 <120911598+ThumbOnAStick@users.noreply.github.com>
2024-11-04 11:00:27 +1000

    Merge branch 'main' of https://github.com/ThumbOnAStick/RimworldTerminator

---
+++ Source/CJTerminator/SitePartWorker_UniqueMechSite.cs
@@ -0,0 +1,74 @@
+﻿using RimWorld;
+using RimWorld.BaseGen;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Terminator
+{
+    public class SitePartWorker_UniqueMechSite : SitePartWorker
+    {
+        private int TicksUntilRaid = 625;
+        private int currentTicks = 0;
+        private bool tick = false;
+
+        public override void Init(Site site, SitePart sitePart)
+        {
+            base.Init(site, sitePart);
+        }
+        public override void SitePartWorkerTick(SitePart sitePart)
+        {
+            base.SitePartWorkerTick(sitePart);
+            if (tick)
+            {
+                currentTicks++;
+                if (currentTicks == TicksUntilRaid)
+                {
+                    tick = false;
+                    IncidentParms incidentParms = new IncidentParms();
+                    incidentParms.target = sitePart.site.Map;
+                    incidentParms.raidArrivalMode = PawnsArrivalModeDefOf.EdgeWalkIn;
+                    incidentParms.points = sitePart.site.ActualThreatPoints;
+                    incidentParms.raidStrategy = RaidStrategyDefOf.ImmediateAttack;
+                    incidentParms.spawnRotation = Rot4.Random;
+                    incidentParms.faction = Find.FactionManager.RandomRaidableEnemyFaction(true, true, true);
+                    if (incidentParms.faction == Faction.OfEntities)
+                    {
+                        incidentParms.pawnGroupKind = PawnGroupKindDefOf.Fleshbeasts;
+                    }
+                    IncidentDefOf.RaidEnemy.Worker.TryExecute(incidentParms);
+                }
+            }
+
+
+        }
+        public override string GetPostProcessedThreatLabel(Site site, SitePart sitePart)
+        {
+            return (string)("Hostiles".Translate() + ": " + "Unknown".Translate().CapitalizeFirst()) + ("\n" + "Contains".Translate() + ": " + "Unknown".Translate().CapitalizeFirst());
+        }
+        public override void PostMapGenerate(Map map)
+        {
+
+            Site site = map.Parent as Site;
+            foreach (Thing allThing in map.listerThings.AllThings)
+            {
+                if (allThing.def.category == ThingCategory.Item)
+                {
+                    CompForbiddable compForbiddable = allThing.TryGetComp<CompForbiddable>();
+                    if (compForbiddable != null && !compForbiddable.Forbidden)
+                    {
+                        allThing.SetForbidden(value: true, warnOnFail: false);
+                    }
+
+
+
+                }
+            }
+        }
+    }
+}
---
+++ Source/CJTerminator/WorkGiver_RestoreJunk.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.AI;
+using Verse;
+using VFECore;
+using RimWorld;
+
+namespace Terminator
+{
+    public class CJ_WorkGiver_RestoreJunk : WorkGiver_Scanner
+    {
+        public override PathEndMode PathEndMode => PathEndMode.Touch;
+
+        public override Danger MaxPathDanger(Pawn pawn)
+        {
+            return Danger.Deadly;
+        }
+
+        public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn)
+        {
+            IEnumerable<Thing> workThings = Find.CurrentMap.listerThings.AllThings.Where((x) => { return x.def.defName == "CJ_AncientTerminatorJunk"; });
+            return workThings;
+        }
+
+        public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            if (!pawn.CanReserveAndReach(t, PathEndMode, MaxPathDanger(pawn), 1, 1, null, forced))
+            {
+                return false;
+            }
+            if (pawn.mechanitor == null)
+            {
+                return false;
+            }
+            if (t.def == Definitions.CJ_AncientTerminatorJunk)
+            {
+                List<Thing> things = pawn.MapHeld.listerThings.ThingsOfDef(ThingDefOf.Plasteel);
+                if (!things.Where((c) => { return c.stackCount > 100; }).Any() && things.Count() == 75)
+                {
+                    return false;
+                }
+            }
+
+
+            if (t.Map.designationManager.DesignationOn(t, Definitions.CJ_RestoreJunk) == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            Job job;
+            
+            //Find.CurrentMap.listerThings.ThingsOfDef(ThingDefOf.Plasteel).First();
+            if (t.def == Definitions.CJ_AncientTerminatorJunk)
+            {
+                Thing thing = GenClosest.ClosestThing_Global_Reachable(t.PositionHeld, t.MapHeld, pawn.MapHeld.listerThings.ThingsOfDef(ThingDefOf.Plasteel), PathEndMode.ClosestTouch, TraverseParms.For(pawn), 9999f);
+                job = JobMaker.MakeJob(Definitions.CJ_RestoreMechJunk, t, thing);
+                job.count = 2;
+            }
+
+            return job;
+
+        }
+    }
+}
